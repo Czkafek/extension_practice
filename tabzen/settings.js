3a8f045f1102f985cc9ccf16613fb2ff42b66301
Czkafek <czkafek@gmail.com>
2025-05-03 13:23:06 +0200

    changing mode of blockade implemented

--- tabzen/settings.js
+++ tabzen/settings.js
@@ -31,8 +31,8 @@ async function init() {
             type = "text";
         }
         else {
-            select = `<option value="całkowity">całkowity</option>
-                            <option value="przepustka" selected>przepustka</option>`;
+            select =    `<option value="całkowity">całkowity</option>
+                        <option value="przepustka" selected>przepustka</option>`;
             isDisabled = "";
             type = "number";
         }
@@ -48,11 +48,11 @@ async function init() {
                     </div>`;*/
         string += `<div class="line"> 
                         <input type="text" value="${element.domain}">
-                        <select id="tryb">
+                        <select class="tryb">
                             ${select}
                         </select>
                         <input type="${type}" ${isDisabled} value="${element.amount}">
-                        <div class="field">
+                        <div class="field"> 
                             <input type="${type}" ${isDisabled} value="${element.enterSpan}">
                             <p>minut</p>
                         </div>
@@ -60,7 +60,7 @@ async function init() {
                             <input type="${type}" ${isDisabled} value="${element.breakSpan}">
                             <p>sekund</p>
                         </div>
-                        <div class="img" data-key="${element.domain}"><img src="icons/trash.svg" alt=""></div>
+                        <div class="img" data-key="${element.id}"><img src="icons/trash.svg" alt=""></div>
                     </div>`
     });
 
@@ -73,16 +73,16 @@ async function init() {
 
     document.getElementById("submit").addEventListener("click", (e) => {
         const domain = (document.getElementById("addDomain").value);
-        console.log(domain);
         if (domain != "") {
+            const newId = Date.now();
             list.push({
+                id: newId,
                 domain: domain,
                 mode: "przepustka",
                 amount: "10",
                 enterSpan: "10",
                 breakSpan: "10"
             })
-            console.log(list);
             chrome.storage.local.set({ list }, () => {
                 console.log("Dodano i zapisano nową domenę");
                 init();
@@ -93,13 +93,95 @@ async function init() {
     document.querySelectorAll(".img").forEach(btn => {
         btn.addEventListener("click", (e) => {
             const key = e.currentTarget.dataset.key;
-            console.log(key);
-            list = list.filter( element => element.domain !== key);
+            list = list.filter( element => element.id != key );
+            console.log()
             chrome.storage.local.set({ list }, () => {
                 console.log("Usunięto i zapisano listę domen");
                 init();
             })
         });
+    });
+
+    document.querySelectorAll(".tryb").forEach(element => {
+        element.addEventListener("change", e => {
+            const container = e.target.parentElement;
+            const allChildren = container.querySelectorAll("input");
+            const trash = container.querySelectorAll(".img");
+            if(e.target.value === "całkowity") {
+                list = list.map(element => {
+                    if(element.id == trash[0].dataset.key) {
+                        element = {
+                            id: trash[0].dataset.key,
+                            domain: allChildren[0].value,
+                            mode: "całkowity",
+                            amount: "brak",
+                            enterSpan: "brak",
+                            breakSpan: "brak"
+                        }
+                    }
+                    return element;
+                })
+            } else {
+                list = list.map(element => {
+                    if(element.id == trash[0].dataset.key) {
+                        element = {
+                            id: trash[0].dataset.key,
+                            domain: allChildren[0].value,
+                            mode: "przepustka",
+                            amount: "10",
+                            enterSpan: "10",
+                            breakSpan: "10"
+                        }
+                    }
+                    return element;
+                })
+            }
+            chrome.storage.local.set({ list }, () => {
+                console.log("Zmieniono typ domeny i zapisano listę");
+                init();
+            })
+            /*
+            let string;
+            if(e.target.value === "całkowity") {
+                string =    `<div class="line">
+                                    <input type="text" value="${allChildren[0].value}">
+                                    <select class="tryb">
+                                        <option value="całkowity" selected>całkowity</option>
+                                        <option value="przepustka">przepustka</option>
+                                    </select>
+                                    <input type="text" disabled value="brak">
+                                    <div class="field"> 
+                                        <input type="text" disabled value="brak">
+                                        <p>minut</p>
+                                    </div>
+                                    <div class="field">
+                                        <input type="text" disabled value="brak">
+                                        <p>sekund</p>
+                                    </div>
+                                    <div class="img" data-key="${trash[0].dataset.key}"><img src="icons/trash.svg" alt=""></div>
+                                </div>`
+            } else {
+                console.log("Hej");
+                string =    `<div class="line">
+                                    <input type="text" value="${allChildren[0].value}">
+                                    <select class="tryb">
+                                        <option value="całkowity">całkowity</option>
+                                        <option value="przepustka" selected>przepustka</option>
+                                    </select>
+                                    <input type="number" disabled value="10">
+                                    <div class="field"> 
+                                        <input type="number" disabled value="10">
+                                        <p>minut</p>
+                                    </div>
+                                    <div class="field">
+                                        <input type="number" disabled value="10">
+                                        <p>sekund</p>
+                                    </div>
+                                    <div class="img" data-key="${trash[0].dataset.key}"><img src="icons/trash.svg" alt=""></div>
+                                </div>`
+            }
+            container.innerHTML = string;*/
+        })
     })
 }
 
